ddd44ec92831baf82bcdde6dc1476ec8ebd56c80
Kuilto <Kuilto@users.noreply.github.com>
2020-12-24 01:18:20 -0500

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="bg-black">
+    <div class="container mx-auto flex items-center h-full px-2">
+      <NuxtLink to="/" class="text-white">Logo </NuxtLink>
+      <div class="ml-auto space-x-2">
+        <NuxtLink to="/" class="text-white">Home </NuxtLink>
+        <NuxtLink to="/about" class="text-white">About </NuxtLink>
+        <NuxtLink to="/" class="text-white">Projects </NuxtLink>
+        <NuxtLink to="/" class="text-white">My story </NuxtLink>
+      </div>
+    </div>
+  </nav>
+</template>
+<style lang="postcss">
+nav {
+  height: 70px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <section
+      :style="{
+        backgroundImage: actualImage,
+      }"
+      class="hero flex items-center justify-center bg-cover bg-center bg-no-repeat relative"
+    >
+      <span
+        class="absolute left-0 text-white bg-red-800"
+        @click="
+          currentIndex =
+            currentIndex - 1 < 0 ? images.length - 1 : currentIndex - 1
+        "
+      >
+        back
+      </span>
+      <span
+        class="absolute right-0 text-white bg-red-800"
+        @click="currentIndex = (currentIndex + 1) % images.length"
+      >
+        next
+      </span>
+      <ul class="absolute flex bottom-4 space-x-4">
+        <li
+          class="w-5 h-5 border-10 border-opacity-80 rounded-full"
+          v-for="(image, index) in images"
+          :class="{
+            'bg-blue-800 border-gray-200': index == currentIndex,
+            'bg-gray-300 border-gray-300': index != currentIndex,
+          }"
+          :key="index"
+          @click="currentIndex = index"
+        ></li>
+      </ul>
+    </section>
+    <section class="h-screen bg-red-200"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      images: [
+        "https://www.asus.com/media/CAen/banners/u7qjrikyo7l2o4tz/u7qjrikyo7l2o4tz-0_0_desktop.jpg",
+        "https://www.asus.com/media/CAen/banners/9hqzmajpyxkjbjoy/9hqzmajpyxkjbjoy-0_0_desktop.jpg",
+        "https://www.asus.com/media/CAen/banners/ftbkospo8kesddn8/ftbkospo8kesddn8-0_0_desktop.jpg",
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    actualImage() {
+      return `url(${this.images[this.currentIndex]})`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hero {
+  height: calc(100vh - 70px);
+}
+</style>
